3347708fcefa5a038d97ea034c82b6e6a3e53e80
Abul Hassan <imabulhasan99@gmail.com>
2025-08-31 21:07:40 +0600

    Add navigation items configuration to FilamentMailerLite plugin, including options to enable or disable default navigation items.

--- src/FilamentMailerLite.php
+++ src/FilamentMailerLite.php
@@ -3,6 +3,7 @@
 namespace Ihasan\FilamentMailerLite;
 
 use Filament\Contracts\Plugin;
+use Filament\Navigation\NavigationItem;
 use Filament\Panel;
 use Filament\Support\Concerns\EvaluatesClosures;
 
@@ -11,6 +12,8 @@ class FilamentMailerLite implements Plugin
     use EvaluatesClosures;
 
     protected string $navigationGroup = 'MailerLite';
+    protected array $navigationItems = [];
+    protected bool $hasDefaultNavigationItems = true;
 
     public function getId(): string
     {
@@ -19,7 +22,41 @@ class FilamentMailerLite implements Plugin
 
     public function register(Panel $panel): void
     {
-        // Register any resources, pages, or widgets here
+        $navigationItems = $this->navigationItems;
+        
+        if ($this->hasDefaultNavigationItems) {
+            $defaultItems = [
+                NavigationItem::make('MailerLite Dashboard')
+                    ->url('#')
+                    ->icon('heroicon-o-envelope')
+                    ->group($this->getNavigationGroup())
+                    ->sort(1),
+                
+                NavigationItem::make('Subscribers')
+                    ->url('#')
+                    ->icon('heroicon-o-users')
+                    ->group($this->getNavigationGroup())
+                    ->sort(2),
+                    
+                NavigationItem::make('Campaigns')
+                    ->url('#')
+                    ->icon('heroicon-o-megaphone')
+                    ->group($this->getNavigationGroup())
+                    ->sort(3),
+                    
+                NavigationItem::make('Settings')
+                    ->url('#')
+                    ->icon('heroicon-o-cog-6-tooth')
+                    ->group($this->getNavigationGroup())
+                    ->sort(4),
+            ];
+            
+            $navigationItems = array_merge($defaultItems, $navigationItems);
+        }
+        
+        if (!empty($navigationItems)) {
+            $panel->navigationItems($navigationItems);
+        }
     }
     
     public function boot(Panel $panel): void
@@ -47,6 +84,27 @@ class FilamentMailerLite implements Plugin
     public function getNavigationGroup(): string
     {
         return $this->evaluate($this->navigationGroup) ?? 'MailerLite';
+    }
+
+    public function navigationItems(array $items): static
+    {
+        $this->navigationItems = $items;
+
+        return $this;
+    }
+
+    public function disableDefaultNavigationItems(): static
+    {
+        $this->hasDefaultNavigationItems = false;
+
+        return $this;
+    }
+
+    public function enableDefaultNavigationItems(): static
+    {
+        $this->hasDefaultNavigationItems = true;
+
+        return $this;
     }
 
 }
