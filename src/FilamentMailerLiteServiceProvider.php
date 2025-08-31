caba1f5b5a960190ec25d156d77cc88c443c6b2d
Abul Hassan <imabulhasan99@gmail.com>
2025-08-31 21:02:27 +0600

    Refactor FilamentMailerLiteServiceProvider to simplify package configuration by removing unused methods and comments.

--- src/FilamentMailerLiteServiceProvider.php
+++ src/FilamentMailerLiteServiceProvider.php
@@ -10,16 +10,8 @@ class FilamentMailerLiteServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-mailerlite')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_filament_mailerlite_table')
-            ->hasCommand(FilamentMailerLiteCommand::class);
+            ->hasConfigFile('filament-mailerlite');
     }
 }
