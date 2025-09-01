2b07116269dc0e728f14613d52732113e464cd29
Abul Hassan <imabulhasan99@gmail.com>
2025-09-01 09:24:22 +0600

    Implement enhanced CreateSubscriber page with new actions for importing from MailerLite and improved notification messages. Update UI elements for better user experience, including custom labels and icons for create and cancel actions.

--- src/Resources/SubscriberResource/Pages/CreateSubscriber.php
+++ src/Resources/SubscriberResource/Pages/CreateSubscriber.php
@@ -2,13 +2,56 @@
 
 namespace Ihasan\FilamentMailerLite\Resources\SubscriberResource\Pages;
 
+use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Ihasan\FilamentMailerLite\Resources\SubscriberResource;
 use Filament\Notifications\Notification;
+use Filament\Support\Enums\MaxWidth;
 
 class CreateSubscriber extends CreateRecord
 {
     protected static string $resource = SubscriberResource::class;
+
+    protected static ?string $title = 'Add New Subscriber';
+
+    public function getMaxContentWidth(): MaxWidth
+    {
+        return MaxWidth::FiveExtraLarge;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('importFromMailerLite')
+                ->label('Import from MailerLite')
+                ->icon('heroicon-o-cloud-arrow-down')
+                ->color('info')
+                ->outlined()
+                ->action(function () {
+                    Notification::make()
+                        ->title('Import Feature')
+                        ->body('Import functionality can be implemented to pull existing subscribers from MailerLite.')
+                        ->info()
+                        ->send();
+                }),
+        ];
+    }
+
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+            ->label('Create & Sync with MailerLite')
+            ->icon('heroicon-o-plus-circle')
+            ->color('success');
+    }
+
+    protected function getCancelFormAction(): Actions\Action
+    {
+        return parent::getCancelFormAction()
+            ->label('Cancel')
+            ->icon('heroicon-o-x-mark')
+            ->color('gray');
+    }
 
     protected function afterCreate(): void
     {
@@ -16,15 +59,23 @@ class CreateSubscriber extends CreateRecord
             $this->record->syncWithMailerLite();
             
             Notification::make()
-                ->title('Subscriber created and synced with MailerLite!')
+                ->title('Subscriber Created Successfully!')
+                ->body("Added {$this->record->email} to your MailerLite account and local database.")
                 ->success()
+                ->duration(5000)
                 ->send();
         } catch (\Exception $e) {
             Notification::make()
-                ->title('Subscriber created but sync failed')
-                ->body('The subscriber was created locally but could not be synced with MailerLite: ' . $e->getMessage())
+                ->title('Partial Success')
+                ->body('Subscriber was created locally but could not be synced with MailerLite: ' . $e->getMessage())
                 ->warning()
+                ->duration(8000)
                 ->send();
         }
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
     }
 }
