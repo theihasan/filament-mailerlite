40dc45dd9ca0e66b2b02a15f5fdca9f7e4da93e3
Abul Hassan <imabulhasan99@gmail.com>
2025-09-02 21:35:58 +0600

    Add syncWithMailerLite method to Group model and implement afterCreate hook in CreateGroup page for MailerLite integration.

--- src/Models/Group.php
+++ src/Models/Group.php
@@ -3,6 +3,7 @@
 namespace Ihasan\FilamentMailerLite\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Ihasan\LaravelMailerlite\Facades\MailerLite;
 
 class Group extends Model
 {
@@ -28,6 +29,33 @@ class Group extends Model
     public function getTable()
     {
         return 'mailerlite_groups';
+    }
+
+    /**
+     * Sync group with MailerLite
+     */
+    public function syncWithMailerLite(): array
+    {
+        try {
+            $data = [
+                'name' => $this->name,
+                'description' => $this->description,
+            ];
+
+            if ($this->mailerlite_id) {
+                $result = MailerLite::groups()->update($this->mailerlite_id, $data);
+            } else {
+                $result = MailerLite::groups()->create($data);
+                
+                if (isset($result['id'])) {
+                    $this->update(['mailerlite_id' => $result['id']]);
+                }
+            }
+            
+            return $result;
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to sync with MailerLite: ' . $e->getMessage());
+        }
     }
 }
 
